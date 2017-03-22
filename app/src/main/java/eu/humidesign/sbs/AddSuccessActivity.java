c8654720c9b4dc8c807af663b51874757f10e1b5
humi <humi777@outlook.com>
2017-03-22 15:33:27 +0100

    small bit of progress

--- app/src/main/java/eu/humidesign/sbs/AddSuccessActivity.java
+++ app/src/main/java/eu/humidesign/sbs/AddSuccessActivity.java
@@ -6,6 +6,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddSuccessActivity extends AppCompatActivity {
 
@@ -13,11 +17,6 @@ public class AddSuccessActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_success);
-
-        EditText myEditText = new EditText(this);
-        myEditText.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        LinearLayout myLayout = (LinearLayout) findViewById(R.id.listasukcesow);
-        myLayout.addView(myEditText);
 
 
         final Button addrowbutton = (Button) findViewById(R.id.add_row_button);
@@ -27,7 +26,7 @@ public class AddSuccessActivity extends AppCompatActivity {
                 EditText myEditText = new EditText(AddSuccessActivity.this);
                 myEditText.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
                 LinearLayout myLayout = (LinearLayout) findViewById(R.id.listasukcesow);
-                myEditText.setText("success "+(myLayout.getChildCount()+1);
+                myEditText.setText("success "+(myLayout.getChildCount()+1));
                 myLayout.addView(myEditText);
 
             }
@@ -37,9 +36,16 @@ public class AddSuccessActivity extends AppCompatActivity {
         donebutton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-
+                LinearLayout myLayout = (LinearLayout) findViewById(R.id.listasukcesow);
+                int total = myLayout.getChildCount();
+                List<String> sukcesy = new ArrayList();
+                for (int i =0;i<total;i++) {
+                    EditText myEditText = (EditText) myLayout.getChildAt(i);
+                    sukcesy.add(myEditText.getText().toString());
+                }
+                String happymsg = "Will store " + String.valueOf(sukcesy.size());
+                Toast.makeText(AddSuccessActivity.this, happymsg , Toast.LENGTH_LONG).show();
                 finish();
-
             }
         });
 
